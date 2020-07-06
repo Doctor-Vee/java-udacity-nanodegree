48f958e58934ac2f6f2f2ccf32e8d1798229e308
Doctor Vee <victor.chinewubeze@gmail.com>
2020-07-06 21:11:00 +0100

    solve topKLarger

--- src/part01/lesson01_java_refresher_course/JavaProgrammingPracticeExercises.java
+++ src/part01/lesson01_java_refresher_course/JavaProgrammingPracticeExercises.java
@@ -1,11 +1,14 @@
 package part01.lesson01_java_refresher_course;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class JavaProgrammingPracticeExercises {
+    /*
+    findDuplicate
+    Complete find the first duplicate character in a string.
+    Complete the following method so given a string it returns the index of the first duplicated character or -1 if there are no duplicated characters.
+     */
     public static int findDuplicate(String input) {
         List<Character> characters = new ArrayList<>();
         int index = 0;
@@ -30,6 +33,11 @@ public class JavaProgrammingPracticeExercises {
 //        return -1;
 //    }
 
+
+    /*
+    Complete the “two sum” coding problem.
+    Given an integer array, return true if two numbers in this array can be summed to target.
+     */
 //    public static boolean twoSum(int[] nums, int target) {
 //        for(int i = 0; i < nums.length; i++){
 //            for (int j = i+1; j < nums.length; j++){
@@ -51,12 +59,17 @@ public class JavaProgrammingPracticeExercises {
         return false;
     }
 
+
+    /*
+    Complete the “String reverse” coding problem.
+    Write a function that takes a string as input and returns the string reversed.
+     */
     public static String reverseString(String s) {
-        String reversed = "";
-        for (int i = s.length()-1; i>=0; i--){
-            reversed += s.charAt(i);
+        StringBuilder reversed = new StringBuilder(); //The IDE advised me to use StringBuilder instead of just using String which I used earlier
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed.append(s.charAt(i));
         }
-        return reversed;
+        return reversed.toString();
     }
 
 //    // Their Solution
@@ -68,10 +81,43 @@ public class JavaProgrammingPracticeExercises {
 //        return sb.toString();
 //    }
 
+
+    /*
+    Complete the “find top k larger number” in an unsorted integer array.
+    Given an unsorted integer array, return the top k larger number in a sorted list. The k is always less than array size. Make sure the time complexity is less than O(nlogn).
+     */
+//public static int[] topKLarger(int [] array, int k){
+//        Arrays.sort(array);
+//        for (int i = 0; i < array.length/2; i++){
+//            int temp = array[i];
+//            array[i] = array[array.length - 1 - i];
+//            array[array.length - 1 - i] = temp;
+//        }
+//        return Arrays.copyOf(array, k);
+//}
+
+    public static int[] topKLarger(int [] array, int k){
+        Arrays.sort(array);
+        return Arrays.copyOfRange(array, array.length - k, array.length);
+    }
+
+//    // Idea from their Solution
+//    public static List<Integer> topKLarger(int[] array, int k) {
+//        TreeSet<Integer> arraySet = new TreeSet<>();
+//        for (int num : array) {
+//            arraySet.add(num);
+//            if (arraySet.size() > k) arraySet.pollFirst();
+//        }
+//        return arraySet.stream().collect(Collectors.toList());
+//    }
+
+
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(findDuplicate("abcd"));
-        System.out.println(twoSum(new int[]{1,4,5,1,6}, 12));
+        System.out.println(twoSum(new int[]{1, 4, 5, 1, 6}, 12));
         System.out.println(reverseString("abcde"));
+        System.out.println(Arrays.toString(topKLarger(new int[]{2, 11, 5, 4, 7, 3, 8, 9}, 3))); // for my solution
+//        System.out.println(topKLarger(new int[]{2, 11, 5, 4, 7, 3, 8, 9}, 5)); // for their solution
     }
 }
